804f297834ca1d4bf80ad7a343a0283525852562
Deepan <deepan.seeralan@gmail.com>
2018-08-06 19:48:23 -0400

    added linear regression cost and gradient descent

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,15 +19,31 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+% cost function of a linear regression
+%
+% J(theta) = ((1/(2*m)) * sum(h(x) - y)^2) + (lambda/(2*m)) * sum(theta[2:])^2
 
+% m = length(y)
+% h(x) in linear regression
+% h(x) = theta*x
+% size_of_theta: nx1, size_of_X = mxn
+hofX = X * theta;
+J_without_theta = (1/(2*m)) * sum((hofX .- y) .^ 2);
+reg_parameter = (lambda/(2*m)) * sum(theta([2:end],:) .^ 2);
 
+J = J_without_theta + reg_parameter;
 
+% computing the gradient descent
+% grad0 = (1/m) * sum(h(X) - y) * X 
+% grad = (1/m) * sum(h(X) - y) + (lambda/m) * theta ; j >= 1 (in Octave terms, it is j >= 2)
 
+grad0 = (1/m) * sum((hofX - y) .* X(:,1));
 
-
-
-
-
+% theta is of size nx1; grad dimension is 1xn
+% Hence we have to take transpose of theta([2:end],:) when calculating
+% grad in the below line
+grad = ((1/m) * sum((hofX - y) .* X(:,[2:end]))) + ((lambda/m) .* theta([2:end],:)');
+grad = [grad0 grad];
 
 
 % =========================================================================
